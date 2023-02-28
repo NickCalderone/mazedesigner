ac472481b5ce51c600368d681d72e3291768e8ab
Nicholas Calderone <calderonenick@gmail.com>
2023-02-28 12:23:47 -0700

    Removed uneeded imports
    Removed objectTypeAnnotation and jsxOpeningElement imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { objectTypeAnnotation, jsxOpeningElement } from "@babel/types";
 
 class App extends React.Component {
     constructor(props) {
